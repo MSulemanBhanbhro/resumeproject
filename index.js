7071442a9a8b71f4de7d3aa47d4ab3d566353432
M Suleman <ms5901562@gmail.com>
2024-09-07 16:11:11 -0700

    Updated

---
+++ index.js
@@ -0,0 +1,9 @@
+var skillsection = document.getElementById("skillsection");
+function toggle() {
+    if (skillsection.style.display === "none") {
+        skillsection.style.display = "block";
+    }
+    else {
+        skillsection.style.display = "none";
+    }
+}
---
+++ index.ts
@@ -0,0 +1,8 @@
+var skillsection = document.getElementById("skillsection");
+        function toggle() {
+            if (skillsection .style.display === "none") {
+                skillsection.style.display = "block";
+    } else {
+        skillsection.style.display = "none";
+    }
+        }
